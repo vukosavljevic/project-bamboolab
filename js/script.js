7cda7e81d1bb7614563f11edcae567defde98e04
vukosavljevic <domagojv2005@gmail.com>
2022-08-09 16:07:21 +0200

    fix : hamburger menu scroll

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const navMenu = document.querySelector('.navbar__list');
 const navbar = document.querySelector('.navbar');
 const navbarLogo = document.querySelector('.navbar__png');
 const getButton = document.querySelector('.navbar__item-button');
+const header = document.querySelector('header');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle("navbar-bg-color");
@@ -10,4 +11,5 @@ hamburger.addEventListener('click', () => {
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
     getButton.classList.toggle("active");
+    header.classList.toggle("fixed");
 })
