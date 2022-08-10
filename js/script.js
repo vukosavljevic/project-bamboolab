9ac63be02dcf35127a8dd0cc20f5e37df61858a0
vukosavljevic <domagojv2005@gmail.com>
2022-08-10 14:49:34 +0200

    style: navbar and hero section

--- js/script.js
+++ js/script.js
@@ -4,6 +4,13 @@ const navbar = document.querySelector('.navbar');
 const navbarLogo = document.querySelector('.navbar__png');
 const getButton = document.querySelector('.navbar__item-button');
 const header = document.querySelector('header');
+const hero = document.querySelector('.hero');
+const about = document.querySelector('.about');
+const examples = document.querySelector('.examples');
+const slider = document.querySelector('.slider');
+const contactForm = document.querySelector('.contact-form');
+const information = document.querySelector('.information');
+const footer = document.querySelector('.footer');
 
 hamburger.addEventListener('click', () => {
     navbar.classList.toggle("navbar-bg-color");
@@ -12,4 +19,11 @@ hamburger.addEventListener('click', () => {
     navMenu.classList.toggle("active");
     getButton.classList.toggle("active");
     header.classList.toggle("fixed");
+    hero.classList.toggle("deactivate");
+    about.classList.toggle("deactivate");
+    examples.classList.toggle("deactivate");
+    slider.classList.toggle("deactivate");
+    contactForm.classList.toggle("deactivate");
+    information.classList.toggle("deactivate");
+    footer.classList.toggle("deactivate");
 })
